c113a04577b0c2d8cc988910b09c5ea06a287a97
Matthew Crotty <mjc2011@verizon.net>
2019-09-25 14:35:26 -0400

    Barebones Vue + Tabular frontend

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id='app'>
+      <p>{{ message }}<p>
+      <div id='table-test'></div>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'MainPage',
+  data() {
+    return {
+      message: 'Hello World',
+    };
+  },
+};
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,39 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+});
+
+const Tabulator = require('tabulator-tables');
+/* eslint-disable */
+const tabledata = [
+  {id:1, name:"Oli Bob", age:"12", col:"red", dob:""},
+ 	{id:2, name:"Mary May", age:"1", col:"blue", dob:"14/05/1982"},
+ 	{id:3, name:"Christine Lobowski", age:"42", col:"green", dob:"22/05/1982"},
+ 	{id:4, name:"Brendon Philips", age:"125", col:"orange", dob:"01/08/1980"},
+ 	{id:5, name:"Margret Marmajuke", age:"16", col:"yellow", dob:"31/01/1999"},
+];
+const table = new Tabulator('#table-test', {
+  height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
+ 	data:tabledata, //assign data to table
+ 	layout:"fitColumns", //fit columns to width of table (optional)
+ 	columns:[ //Define Table Columns
+	 	{title:"Name", field:"name", width:150},
+	 	{title:"Age", field:"age", align:"left", formatter:"progress"},
+	 	{title:"Favourite Color", field:"col"},
+	 	{title:"Date Of Birth", field:"dob", sorter:"date", align:"center"},
+ 	],
+ 	rowClick:function(e, row){ //trigger an alert message when the row is clicked
+ 		alert("Row " + row.getData().id + " Clicked!!!!");
+ 	},
+});
